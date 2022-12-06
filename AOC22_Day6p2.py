b06ab17291b368a6a0a294a344b997c2dac8a2b7
vcoppo23 <vcoppo23@bsmschool.org>
2022-12-06 08:08:52 -0600

    commented day 6

--- AOC22_Day6p2.py
+++ AOC22_Day6p2.py
@@ -1,14 +1,19 @@
-
+import time
+start = time.time()
 with open("/Users/vcoppo23/Desktop/AOC22/inputs/input6.txt", "r") as file:
     stream = file.read()
 
 
 
 for i in range(len(stream)):
+    ##makes code a substrting of stream 14 characters long
     code = stream[i:i+14]
-    if len(code) < 14:
-        break
+    ##makes code a set, which removes duplicates
     code = set(code)
+    ## Checking if any duplicates were removed
     if len(code) == 14:
+        ##if no duplicates were removed, then the code is the answer
         print (code,i+14)
         break
+
+print('total time: ',time.time()-start)
